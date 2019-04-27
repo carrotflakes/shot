09f092c4b137e0a9118967558e1204dde7490d89
carrotflakes <carrotflakes@gmail.com>
2019-04-27 23:43:28 +0900

    wip

--- src/shot.lisp
+++ src/shot.lisp
@@ -3,22 +3,14 @@
   (:import-from :shot.parser
                 :parse)
   (:import-from :shot.transpile
-                :statements
-                :expression))
+                :statements)
+  (:import-from :shot.condition
+                :shot-error
+                :failed)
+  (:export :shot
+           :shot-error
+           :failed))
 (in-package :shot)
 
-(defun f (source)
-  (print source)
-  (let ((ast (parse source)))
-    (print ast)
-    (let ((code (statements ast)))
-      (print code)
-      (print (eval code)))))
-
-(f "1")
-(f "a = 1")
-(f "b = 2 c = 3 {a::b c \"d\": 1 ...{}}")
-(f "{}")
-(f "{a: b c: d ..._} = {a: 1 c: 2 e: true}")
-(f "f x = x + 1")
-(f "[1=true 2=false](1)")
+(defun shot (source)
+  (eval (statements (parse source))))
--- tests/shot.lisp
+++ tests/shot.lisp
@@ -8,6 +8,20 @@
 
 (plan nil)
 
-;; blah blah blah.
+(defun f (source)
+  (print source)
+  (let ((ast (shot.parser:parse source)))
+    (print ast)
+    (let ((code (shot.tranpile:statements ast)))
+      (print code)
+      (print (eval code)))))
+
+(f "1")
+(f "a = 1")
+(f "b = 2 c = 3 {a::b c \"d\": 1 ...{}}")
+(f "{}")
+(f "{a: b c: d ..._} = {a: 1 c: 2 e: true}")
+(f "f x = x + 1")
+(f "[1=true 2=false](1)")
 
 (finalize)
